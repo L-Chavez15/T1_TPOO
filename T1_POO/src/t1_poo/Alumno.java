b74ea71abaaf2bb9508cf15466b8dd16d49675b7
Leonardo Chávez <N00449821@upn.pe>
2026-09-21 20:43:50 -0500

    Creacion de getters y setters

--- T1_POO/src/t1_poo/Alumno.java
+++ T1_POO/src/t1_poo/Alumno.java
@@ -10,7 +10,49 @@ package t1_poo;
  */
 public class Alumno 
 {
+    private String nombre;
+    private String numero_documento;
+    private String Nivel_socie;
+    private String tipo_beca;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumero_documento() {
+        return numero_documento;
+    }
+
+    public void setNumero_documento(String numero_documento) {
+        this.numero_documento = numero_documento;
+    }
+
+    public String getNivel_socie() {
+        return Nivel_socie;
+    }
+
+    public void setNivel_socie(String Nivel_socie) {
+        this.Nivel_socie = Nivel_socie;
+    }
+
+    public String getTipo_beca() {
+        return tipo_beca;
+    }
+
+    public void setTipo_beca(String tipo_beca) {
+        this.tipo_beca = tipo_beca;
+    }
+    
+    
+    
+    
+    
    public void VerDato()
    {
+       
    }
 }
